5bf0200a33e3193a99f7b7caae0f01d1b0b6b65a
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2022-08-04 17:24:47 -0300

    lista 01 - exercicio 02

--- src/main/java/sptech/exercicios/exercicios01/ex01/Exercicio02.java
+++ src/main/java/sptech/exercicios/exercicios01/ex01/Exercicio02.java
@@ -4,7 +4,28 @@ public class Exercicio02 {
     
     public static void main(String[] args) {
 
+        int[] array = {7, 3, 5, 2};
+
+        System.out.println(imperativeSum(array));
+        System.out.println(recursiveSum(array));
     }
 
-    
+    public static int imperativeSum(int[] array) {
+        int sum = 0;
+        for (int value : array) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static int recursiveSum(int[] array, int start) {
+        if (start < array.length) {
+            return array[start] + recursiveSum(array, start + 1);
+        }
+        return 0;
+    }
+    public static int recursiveSum(int[] array) {
+        return recursiveSum(array, 0);
+    }
+
 }
