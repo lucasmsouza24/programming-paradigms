25e9dc7ef69168e71ab89025244c036f79a87adc
lucasmsouza24 <lucasmesquita58@hotmail.com>
2022-08-05 00:30:32 -0300

    lista 01 - exercício 04

---
+++ src/main/java/sptech/exercicios/exercicios01/ex01/Exercicio04.java
@@ -0,0 +1,37 @@
+package sptech.exercicios.exercicios01.ex01;
+
+public class Exercicio04 {
+    
+    public static void main(String[] args) {
+
+        // objective: return amount of odd values in integer array
+        int[] values = {7, 3, 5, 2};
+
+        System.out.println(imperativeAmountOfOdds(values));
+        System.out.println(recursiveAmountOfOdds(values));
+    }
+
+    // imperative
+    public static int imperativeAmountOfOdds(int[] array) {
+        int amount = 0;
+        for (int value : array) {
+            if (value % 2 == 1) {
+                amount++;
+            }
+        }
+        return amount;
+    }
+
+    // recursive
+    public static int recursiveAmountOfOdds(int[] array, int start) {
+        if (start < array.length) {
+            System.out.println("valor: " + array[start]);
+            return array[start] % 2 == 1 ? 1 + recursiveAmountOfOdds(array, start + 1) : recursiveAmountOfOdds(array, start + 1);
+        }
+        return 0;
+    }
+    public static int recursiveAmountOfOdds(int[] array) {
+        return recursiveAmountOfOdds(array, 0);
+    }
+
+}
