7cf6c12ebed61f307a578679b4aba5e2d7d47568
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2022-08-17 00:52:40 -0300

    merge sort imcomplete

--- src/main/java/sptech/exercicios/sortalgorithms/App.java
+++ src/main/java/sptech/exercicios/sortalgorithms/App.java
@@ -6,9 +6,11 @@ public class App {
         
         int[] unsorted = {3, 2, 4, 1, 5, 2, 6};
         int[] insert = InsertSort.sort(unsorted);
+        int[] merge = MergeSort.sort(unsorted);
 
         displayValues(unsorted, "Unsorted");
         displayValues(insert, "Insert");
+        displayValues(merge, "Merge");
     }
 
     public static void displayValues(int[] array, String title) {
---
+++ src/main/java/sptech/exercicios/sortalgorithms/MergeSort.java
@@ -0,0 +1,52 @@
+package sptech.exercicios.sortalgorithms;
+
+public class MergeSort {
+    
+    public static int[] sort(int[] unsorted) {
+        int[] sorted = unsorted.clone();
+
+        sorted = sort(0, unsorted.length - 1, unsorted);
+
+        return sorted;
+    }
+
+    public static int[] sort(int p, int r, int[] v) {
+        if(p > r - 1) {
+            int q = (p + r) / 2;
+            sort(p, q, v);
+            sort(q, r, v);
+            intercala(p, q, r, v);
+        }
+
+        return v;
+    }
+
+    public static void intercala(int p, int q, int r, int[] v) {
+        int i = p;
+        int j = q;
+        int k = 0;
+        int[] w = {};
+
+        while((i < q) && (j < r)) {
+            if(w[k++] <= v[i++]) {
+                w[k++] = v[i++];
+            } else {
+                w[k++] = v[j++];
+            }
+
+            while(i < q) {
+                w[k++] = v[i++];
+            }
+
+            while(j < r) {
+                w[k++] = v[j++];
+            }
+
+            for(i = p; i <= r-1; i++) {
+                v[i] = w[i - p];
+            }
+        }
+
+    }
+
+}
