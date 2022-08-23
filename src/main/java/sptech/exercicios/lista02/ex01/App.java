fc9101900bfdd04f6a1f2f44ff984d3044ada46f
Lucas Mesquita de Souza <lucasmesquita58@hotmail.com>
2022-08-23 01:14:10 -0300

    lista 02 exercicio 01

--- src/main/java/sptech/exercicios/lista02/ex01/App.java
+++ src/main/java/sptech/exercicios/lista02/ex01/App.java
@@ -1,21 +1,69 @@
 package sptech.exercicios.lista02.ex01;
 
+import sptech.exercicios.sortalgorithms.QuickSort;
+
 public class App {
     
     public static void main(String[] args) {
-        DuasPilhas<Integer> stack = new DuasPilhas<Integer>();
-        stack.push(-1, 1);
-        stack.push(-2, 1);
-        stack.push(-3, 1);
-        stack.push(1, 2);
-        stack.push(2, 2);
-        stack.push(3, 2);
-        stack.push(3, 2);
-        stack.push(3, 2);
-        System.out.println(stack.isFull());
-        // stack.push(3, 2);
+        DuasPilhas<Integer> valoresDesordenados = new DuasPilhas<Integer>(10);
+        valoresDesordenados.push(-13, 1);
+        valoresDesordenados.push(11, 1);
+        valoresDesordenados.push(-9, 1);
+        valoresDesordenados.push(6, 1);
+        valoresDesordenados.push(-5, 1);
+        valoresDesordenados.push(2, 2);
+        valoresDesordenados.push(-1, 2);
+        valoresDesordenados.push(8, 2);
+        valoresDesordenados.push(-7, 2);
+        valoresDesordenados.push(3, 2);
 
-        stack.displayAll();
+        valoresDesordenados.displayAll();
+
+        DuasPilhas<Integer> valoresOrdenados = sortValues(valoresDesordenados);
+        // valoresOrdenados.pop(2);
+        valoresOrdenados.displayAll();
+        exibeOrdemInversa(valoresOrdenados);
+    }
+
+    public static DuasPilhas<Integer> sortValues(DuasPilhas<Integer> unsorted) {
+        int[] sorted = new int[unsorted.size()];
+        sorted = QuickSort.sort(sorted);
+        DuasPilhas<Integer> stack = new DuasPilhas<Integer>(unsorted.size());
+
+        int i = 0;
+
+        while(unsorted.isNotEmpty(1)) {
+            sorted[i++] = unsorted.pop(1);
+        }
+        while(unsorted.isNotEmpty(2)) {
+            sorted[i++] = unsorted.pop(2);
+        }
+        
+        for (int value : sorted) {
+            if (value < 0) {
+                stack.push(value, 1);
+            } else {
+                stack.push(value, 2);
+            }
+        }
+
+        return stack;
+    }
+
+    public static void exibeOrdemInversa(DuasPilhas<Integer> stack) {
+        while(stack.isNotEmpty(1)) {
+            System.out.print(stack.pop(1) + ", ");
+        }
+
+        DuasPilhas<Integer> stack2 = new DuasPilhas<Integer>();
+
+        while(stack.isNotEmpty(2)) {
+            stack2.push(stack.pop(2), 2);
+        }
+
+        while(stack2.isNotEmpty(2)) {
+            System.out.print(stack2.pop(2) + ", ");
+        }
     }
 
 }
--- src/main/java/sptech/exercicios/lista02/ex01/DuasPilhas.java
+++ src/main/java/sptech/exercicios/lista02/ex01/DuasPilhas.java
@@ -8,8 +8,8 @@ public class DuasPilhas<T> {
 
     public DuasPilhas(int length) {
         stack = (T[]) new Object[length];
-        top1 = 0;
-        top2 = length - 1;
+        top1 = -1;
+        top2 = length;
     }
     public DuasPilhas() {
         this(10);
@@ -30,18 +30,21 @@ public class DuasPilhas<T> {
 
     // methods
     public boolean isFull() {
-        System.out.println();
         return top1 == top2 - 1;
+    }
+
+    public int size() {
+        return stack.length;
     }
 
     private void push1(T value) {
         if (this.isFull()) throw new IllegalStateException("the Stacks are full");
-        stack[top1++] = value;
+        stack[++top1] = value;
     }    
     
     private void push2(T value) {
         if (this.isFull()) throw new IllegalStateException("the Stacks are full");
-        stack[top2--] = value;
+        stack[--top2] = value;
     }
 
     public void displayArray1() {
@@ -58,5 +61,50 @@ public class DuasPilhas<T> {
         for (int i = 0; i < stack.length; i++) {
             System.out.print(stack[i] + ", ");
         }
+        System.out.println("");
+    }
+
+    public T pop(int vector) {
+        T value;
+        switch (vector) {
+            case 1:
+                value = peek(vector);
+                stack[top1--] = null;
+                return value;
+            case 2:
+                value = peek(vector);
+                stack[top2++] = null;
+                return value;
+            default:
+                throw new IllegalArgumentException("vector " + vector + " is out of range");
+        }
+    }
+
+    public T peek(int vector) {
+        if(this.isEmpty(vector)) throw new IllegalStateException("the stack is empty");
+
+        switch (vector) {
+            case 1:                
+                return stack[top1];
+            case 2:
+                return stack[top2];
+            default:
+                throw new IllegalArgumentException("vector " + vector + " is out of range");
+        }
+    }
+
+    public boolean isEmpty(int vector) {
+        switch (vector) {
+            case 1:
+                return top1 == -1;
+            case 2:
+                return top2 == stack.length;
+            default:
+                throw new IllegalArgumentException("Vector " + vector + " does not exist");
+        }
+    }
+
+    public boolean isNotEmpty(int vector) {
+        return !isEmpty(vector);
     }
 }
